42bb0efcfdd039340bdbb1b6d0aab786e120faf9
krissan <krissan_32@hotmail.com>
2017-10-14 22:11:57 -0400

    test9

--- app/controllers/routines.js
+++ app/controllers/routines.js
@@ -17,12 +17,12 @@ exports.getRoutine = function(req, res, next){
 }
  
 exports.createRoutine = function(req, res, next){
-    var subrouts = Array.from(req.body.subroutines);
+    /*var subrouts = Array.from(req.body.subroutines);
     var readSubrouts = [];
     for (subrout in subrouts)
     {
-        
-    }
+
+    }*/
 
     Routine.create({
         routine : req.body.routineName,
@@ -40,7 +40,7 @@ exports.createRoutine = function(req, res, next){
                 res.send(err);
             }
  
-            res.json(routines + JSON.stringify(req.body.routineName[0]) ++ "++" ++ a);
+            res.json(routines + JSON.stringify(req.body));
 
  
         });
