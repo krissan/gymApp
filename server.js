b997c37176ae8063650e1b58a061bd45831d7c31
krissan <krissan_32@hotmail.com>
2017-11-04 21:22:44 -0400

    heroku deployment

--- server.js
+++ server.js
@@ -10,12 +10,14 @@ var router = require('./app/routes');
  
 mongoose.connect(databaseConfig.url);
  
-app.listen(process.env.PORT || 8080);
-console.log("App listening on port 8080");
- 
 app.use(bodyParser.urlencoded({ extended: false })); // Parses urlencoded bodies
 app.use(bodyParser.json()); // Send JSON responses
 app.use(logger('dev')); // Log requests to API using morgan
 app.use(cors());
- 
+
+app.use(express.static('www'));
+
+app.listen(process.env.PORT || 8080);
+console.log("App listening on port 8080"); 
+
 router(app);
